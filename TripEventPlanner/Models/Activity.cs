3612cc5d14ec918a0653f5ca2225bc20c4954ec4
joshbm-git <joshbm.dev@gmail.com>
2021-05-24 23:54:24 +0200

    Changes

--- TripEventPlanner/Models/Activity.cs
+++ TripEventPlanner/Models/Activity.cs
@@ -11,7 +11,9 @@ namespace TripEventPlanner.Models
         public string Name { get; set; }
         public string Description { get; set; }
         public string Address { get; set; }
+
         public DateTime? StartDate { get; set; }
+
         public DateTime? EndDate { get; set; }
         public decimal? Price { get; set; }
         public string ImageUrl { get; set; }
